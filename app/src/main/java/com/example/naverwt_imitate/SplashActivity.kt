3ed29cca1882e787cdf53515350942515cb26776
louis.kwon <lighter051@gmail.com>
2020-05-26 16:56:51 +0900

    first commit

---
+++ app/src/main/java/com/example/naverwt_imitate/SplashActivity.kt
@@ -0,0 +1,33 @@
+package com.example.naverwt_imitate
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashActivity  : AppCompatActivity(){
+
+    private val SPLASH_TIME_OUT: Long = 3000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+
+        }, SPLASH_TIME_OUT)
+
+    }
+
+    }
+
+
+
+
